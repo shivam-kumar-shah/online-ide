7a5630dd878406505aee292faf5f2768e14423e5
shivam-kumar-shah <shivam-kumar-shah@outlook.com>
2023-09-17 19:44:19 +0530

    style: :art: code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/layout/header";
 import Landing from "./pages/Landing";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+    index: true,
+  },
+  {
+    path: "/editor",
+    element: <Home />,
+  },
+]);
 
 function App() {
   return (
     <div className="app h-screen w-screen grid items-center font-body bg-surface text-font-primary">
-      {/* <Home /> */}
-      <Landing />
+      <RouterProvider router={router} />
     </div>
   );
 }
